be8d37c639016be2de7595ed0a0a876cd64cfa7b
Abhishek Chatterjee <abhishek.chatterjee@optiphoenix.com>
2022-11-30 10:41:41 +0530

    handled async categories fetch using redux-thunk, added spinner while the categories is being fetched

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,17 +1,41 @@
 import { CATEGORIES_ACTION_TYPES } from './category.types';
 
 export const CATEGORIES_INITIAL_STATE = {
-    categories: []
+    categories: [],
+    isLoading: false,
+    error: null
 };
 
+// export const categoriesRedeucer = (state = CATEGORIES_INITIAL_STATE, action) => {
+//     const { type, payload } = action;
+
+//     switch (type) {
+//         case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+//             return {
+//                 ...state,
+//                 categories: payload
+//             };
+//         default:
+//             return state;
+//     }
+// };
 export const categoriesRedeucer = (state = CATEGORIES_INITIAL_STATE, action) => {
     const { type, payload } = action;
 
     switch (type) {
-        case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+            return { ...state, isLoading: true };
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
             return {
                 ...state,
-                categories: payload
+                categories: payload,
+                isLoading: false
+            };
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+            return {
+                ...state,
+                categories: payload,
+                isLoading: false
             };
         default:
             return state;
--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
@@ -18,14 +18,14 @@ import { rootReducer } from './root-reducer';
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
+    whitelist: ['cart']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
+    middleware: [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean)
 });
 
 export const persistor = persistStore(store);
