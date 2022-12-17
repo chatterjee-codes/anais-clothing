35f039e211f04bb30b8459007d0dac9ea0cbb36b
Abhishek Chatterjee <abhishek.chatterjee@optiphoenix.com>
2022-12-18 00:16:58 +0530

    fetchCategoriesAsync thunk to saga migration [shop component]

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+
+import createSagaMiddleWare from 'redux-saga';
+
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
@@ -12,7 +16,7 @@ import { rootReducer } from './root-reducer';
 
 // const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-// // root-reducer
+// root-reducer
 
 // export const store = createStore(rootReducer, undefined, composedEnhancers);
 const persistConfig = {
@@ -20,12 +24,16 @@ const persistConfig = {
     storage,
     whitelist: ['cart']
 };
+const sagaMiddleware = createSagaMiddleWare();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean)
+    // middleware: [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean)
+    middleware: [process.env.NODE_ENV !== 'production' && logger, sagaMiddleware].filter(Boolean)
 });
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
