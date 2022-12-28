97f8db083a7ee81c0ed0f1875e116fba93765b64
Abhishek Chatterjee <abhishek.chatterjee@optiphoenix.com>
2022-12-29 02:15:26 +0530

    optimization: dynamic imports with lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { Routes, Route } from 'react-router-dom';
@@ -9,16 +9,24 @@ import { Routes, Route } from 'react-router-dom';
 //     getCurrentUser
 // } from './utils/firebase/firebase.utils';
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
+// import Home from './routes/home/home.component';
+// import Navigation from './routes/navigation/navigation.component';
+// import Authentication from './routes/authentication/authentication.component';
+// import Shop from './routes/shop/shop.component';
+// import Checkout from './routes/checkout/checkout.component';
 // import { setCurrentUser } from './store/user/user.action';
 import { checkUserSession } from './store/user/user.action';
 
 import { GlobalStyle } from './global.styles';
 import { Fragment } from 'react';
+
+import Spinner from './components/spinner/spinner.component';
+
+const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
+const Home = lazy(() => import('./routes/home/home.component'));
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
+const Authentication = lazy(() => import('./routes/authentication/authentication.component'));
 
 const App = () => {
     const dispatch = useDispatch();
@@ -43,14 +51,16 @@ const App = () => {
     return (
         <Fragment>
             <GlobalStyle />
-            <Routes>
-                <Route path='/' element={<Navigation />}>
-                    <Route index element={<Home />} />
-                    <Route path='shop/*' element={<Shop />} />
-                    <Route path='auth' element={<Authentication />} />
-                    <Route path='checkout' element={<Checkout />} />
-                </Route>
-            </Routes>
+            <Suspense fallback={<Spinner />}>
+                <Routes>
+                    <Route path='/' element={<Navigation />}>
+                        <Route index element={<Home />} />
+                        <Route path='shop/*' element={<Shop />} />
+                        <Route path='auth' element={<Authentication />} />
+                        <Route path='checkout' element={<Checkout />} />
+                    </Route>
+                </Routes>
+            </Suspense>
         </Fragment>
     );
 };
